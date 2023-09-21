af2c6b2174c4e8e4c120cc9b798a661b77715f93
Render <27890173+itsrenderman@users.noreply.github.com>
2023-09-22 00:45:57 +0100

    fix /game

--- hexuscraft-arcade/src/net/hexuscraft/arcade/game/PluginGame.java
+++ hexuscraft-arcade/src/net/hexuscraft/arcade/game/PluginGame.java
@@ -1,10 +1,13 @@
 package net.hexuscraft.arcade.game;
 
 import net.hexuscraft.arcade.Arcade;
+import net.hexuscraft.arcade.game.command.CommandGame;
 import net.hexuscraft.core.MiniPlugin;
+import net.hexuscraft.core.command.PluginCommand;
 import net.hexuscraft.core.permission.IPermission;
 import net.hexuscraft.core.permission.PermissionGroup;
-import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Map;
 
 public class PluginGame extends MiniPlugin {
 
@@ -18,4 +21,9 @@ public class PluginGame extends MiniPlugin {
         PermissionGroup.ADMINISTRATOR._permissions.add(PERM.COMMAND_GAME);
     }
 
+    @Override
+    public final void onLoad(Map<Class<? extends MiniPlugin>, MiniPlugin> dependencies) {
+        final PluginCommand pluginCommand = (PluginCommand) dependencies.get(PluginCommand.class);
+        pluginCommand.register(new CommandGame(this));
+    }
 }
--- hexuscraft-arcade/src/net/hexuscraft/arcade/game/command/CommandGame.java
+++ hexuscraft-arcade/src/net/hexuscraft/arcade/game/command/CommandGame.java
@@ -1,6 +1,5 @@
 package net.hexuscraft.arcade.game.command;
 
-import net.hexuscraft.arcade.Arcade;
 import net.hexuscraft.arcade.game.PluginGame;
 import net.hexuscraft.core.command.BaseMultiCommand;
 
@@ -8,7 +7,7 @@ import java.util.Set;
 
 public class CommandGame extends BaseMultiCommand {
 
-    protected CommandGame(PluginGame game) {
+    public CommandGame(PluginGame game) {
         super(game, "game", "Manage the active game.", Set.of("gm"), PluginGame.PERM.COMMAND_GAME, Set.of());
     }
 
