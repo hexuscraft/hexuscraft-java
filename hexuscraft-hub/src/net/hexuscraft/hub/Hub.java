f94c1fb41278621861f25a4493593b1ff59d6c12
Render <27890173+itsrenderman@users.noreply.github.com>
2023-09-29 12:46:12 +0100

    Auto set default gamemode (though this doesn't seem to be working...)

--- hexuscraft-hub/src/net/hexuscraft/hub/Hub.java
+++ hexuscraft-hub/src/net/hexuscraft/hub/Hub.java
@@ -5,14 +5,21 @@ import net.hexuscraft.hub.player.PluginPlayer;
 import net.hexuscraft.hub.team.PluginTeam;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.world.WorldLoadEvent;
 
 public class Hub extends HexusPlugin {
 
     @Override
     public void load() {
-        register(new PluginPlayer(this));
-        register(new PluginTeam(this));
+        require(new PluginPlayer(this));
+        require(new PluginTeam(this));
 
+        getServer().setDefaultGameMode(GameMode.ADVENTURE);
+    }
+
+    @EventHandler
+    public void on(WorldLoadEvent event) {
         getServer().setDefaultGameMode(GameMode.ADVENTURE);
     }
 
