55b74b69332b3ca2407cb19ba032bcc3447c2cde
Render <27890173+itsrenderman@users.noreply.github.com>
2023-09-29 12:46:54 +0100

    Require the game miniplugin

--- hexuscraft-arcade/src/net/hexuscraft/arcade/Arcade.java
+++ hexuscraft-arcade/src/net/hexuscraft/arcade/Arcade.java
@@ -1,9 +1,13 @@
 package net.hexuscraft.arcade;
 
+import net.hexuscraft.arcade.game.PluginGame;
 import net.hexuscraft.core.HexusPlugin;
 
 public class Arcade extends HexusPlugin {
 
-
+    @Override
+    public final void load() {
+        require(new PluginGame(this));
+    }
 
 }
