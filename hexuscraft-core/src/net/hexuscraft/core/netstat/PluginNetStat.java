13aeda9be5aae892b884c73db276193e7f8af159
Render <27890173+itsrenderman@users.noreply.github.com>
2023-10-29 10:56:08 +0000

    Add permissions for /netstat

--- hexuscraft-core/src/net/hexuscraft/core/netstat/PluginNetStat.java
+++ hexuscraft-core/src/net/hexuscraft/core/netstat/PluginNetStat.java
@@ -4,20 +4,25 @@ import net.hexuscraft.core.MiniPlugin;
 import net.hexuscraft.core.command.PluginCommand;
 import net.hexuscraft.core.netstat.command.CommandNetStat;
 import net.hexuscraft.core.permission.IPermission;
+import net.hexuscraft.core.permission.PermissionGroup;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Map;
 
 public class PluginNetStat extends MiniPlugin {
 
-    public PluginNetStat(JavaPlugin javaPlugin) {
-        super(javaPlugin, "Network Statistics");
-    }
-
     public enum PERM implements IPermission {
         COMMAND_NETSTAT,
         COMMAND_NETSTAT_GROUP,
         COMMAND_NETSTAT_SERVER
+    }
+
+    public PluginNetStat(JavaPlugin javaPlugin) {
+        super(javaPlugin, "Network Statistics");
+
+        PermissionGroup.ADMINISTRATOR._permissions.add(PERM.COMMAND_NETSTAT);
+        PermissionGroup.ADMINISTRATOR._permissions.add(PERM.COMMAND_NETSTAT_GROUP);
+        PermissionGroup.ADMINISTRATOR._permissions.add(PERM.COMMAND_NETSTAT_SERVER);
     }
 
     private PluginCommand _pluginCommand;
