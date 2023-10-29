9f11bb1dd945636c6a857c492b22e25e132120f3
Render <27890173+itsrenderman@users.noreply.github.com>
2023-10-29 10:55:40 +0000

    Improve message construction

--- hexuscraft-core/src/net/hexuscraft/core/buildversion/command/CommandBuildVersion.java
+++ hexuscraft-core/src/net/hexuscraft/core/buildversion/command/CommandBuildVersion.java
@@ -4,33 +4,38 @@ import net.hexuscraft.core.buildversion.PluginBuildVersion;
 import net.hexuscraft.core.chat.F;
 import net.hexuscraft.core.command.BaseCommand;
 import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.PluginDescriptionFile;
 
+import java.util.Map;
 import java.util.Set;
 
 public class CommandBuildVersion extends BaseCommand {
 
-    public CommandBuildVersion(PluginBuildVersion plugin) {
+    public CommandBuildVersion(final PluginBuildVersion plugin) {
         super(plugin, "buildversion", "", "View information about the current plugin.", Set.of("bv"), PluginBuildVersion.PERM.COMMAND_BUILDVERSION);
     }
 
     @Override
-    public final void run(CommandSender sender, String alias, String[] args) {
+    public final void run(final CommandSender sender, final String alias, final String[] args) {
         if (args.length > 0) {
             sender.sendMessage(help(alias));
             return;
         }
 
-        PluginBuildVersion plugin = (PluginBuildVersion) _miniPlugin;
-        long lastModified = plugin.getLastModifiedMillis();
-        long fileSize = plugin.getSizeBytes();
-        sender.sendMessage(F.fMain(this) + "Plugin Information:\n"
-                + F.fMain("") + "Name: " + F.fItem(plugin._javaPlugin.getDescription().getName() + "\n")
-                + F.fMain("") + "Main: " + F.fItem(plugin._javaPlugin.getDescription().getMain() + "\n")
-                + F.fMain("") + "Version: " + F.fItem(plugin._javaPlugin.getDescription().getVersion() + "\n")
-                + F.fMain("") + "Load: " + F.fItem(plugin._javaPlugin.getDescription().getLoad().name() + "\n")
-                + F.fMain("") + "Last Modified: " + F.fItem(F.fTime(System.currentTimeMillis() - lastModified)) + " (" + lastModified + ")\n"
-                + F.fMain("") + "File Size: " + F.fItem(fileSize + " Bytes")
-        );
+        final PluginBuildVersion plugin = (PluginBuildVersion) _miniPlugin;
+        final PluginDescriptionFile description = plugin._javaPlugin.getDescription();
+        final long lastModified = plugin.getLastModifiedMillis();
+
+        final StringBuilder builder = new StringBuilder(F.fMain(this, "Plugin Information:\n"));
+        Map.of(
+                "Name", description.getName(),
+                "Main", description.getMain(),
+                "Version", description.getVersion(),
+                "Load", description.getLoad().name(),
+                "Last Modified", F.fTime(System.currentTimeMillis() - lastModified) + " (" + lastModified + ")",
+                "File Size", plugin.getSizeBytes() + " (B)"
+        ).forEach((key, value) -> builder.append(F.fMain("", key, ": ", F.fItem(value), "\n")));
+        sender.sendMessage(builder.toString());
     }
 
 }
