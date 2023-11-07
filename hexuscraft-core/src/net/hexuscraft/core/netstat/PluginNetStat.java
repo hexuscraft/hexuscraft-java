ed61eef5be9823dab4c552fa674e0b61d5cb0887
Render <27890173+itsrenderman@users.noreply.github.com>
2023-11-07 16:22:30 +0000

    Add plugindatabase support

--- hexuscraft-core/src/net/hexuscraft/core/netstat/PluginNetStat.java
+++ hexuscraft-core/src/net/hexuscraft/core/netstat/PluginNetStat.java
@@ -2,6 +2,7 @@ package net.hexuscraft.core.netstat;
 
 import net.hexuscraft.core.MiniPlugin;
 import net.hexuscraft.core.command.PluginCommand;
+import net.hexuscraft.core.database.PluginDatabase;
 import net.hexuscraft.core.netstat.command.CommandNetStat;
 import net.hexuscraft.core.permission.IPermission;
 import net.hexuscraft.core.permission.PermissionGroup;
@@ -14,6 +15,7 @@ public class PluginNetStat extends MiniPlugin {
     public enum PERM implements IPermission {
         COMMAND_NETSTAT,
         COMMAND_NETSTAT_GROUP,
+        COMMAND_NETSTAT_GROUP_DELETE,
         COMMAND_NETSTAT_SERVER
     }
 
@@ -22,19 +24,22 @@ public class PluginNetStat extends MiniPlugin {
 
         PermissionGroup.ADMINISTRATOR._permissions.add(PERM.COMMAND_NETSTAT);
         PermissionGroup.ADMINISTRATOR._permissions.add(PERM.COMMAND_NETSTAT_GROUP);
+        PermissionGroup.ADMINISTRATOR._permissions.add(PERM.COMMAND_NETSTAT_GROUP_DELETE);
         PermissionGroup.ADMINISTRATOR._permissions.add(PERM.COMMAND_NETSTAT_SERVER);
     }
 
     private PluginCommand _pluginCommand;
+    private PluginDatabase _pluginDatabase;
 
     @Override
     public void onLoad(Map<Class<? extends MiniPlugin>, MiniPlugin> dependencies) {
         _pluginCommand = (PluginCommand) dependencies.get(PluginCommand.class);
+        _pluginDatabase = (PluginDatabase) dependencies.get(PluginDatabase.class);
     }
 
     @Override
     public void onEnable() {
-        _pluginCommand.register(new CommandNetStat(this));
+        _pluginCommand.register(new CommandNetStat(this, _pluginDatabase));
     }
 
 }
