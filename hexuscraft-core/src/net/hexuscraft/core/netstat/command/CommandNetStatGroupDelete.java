2c5dc44842506c988203590f16ac2507b62a9999
Render <27890173+itsrenderman@users.noreply.github.com>
2023-11-07 16:21:53 +0000

    Add /net g d

---
+++ hexuscraft-core/src/net/hexuscraft/core/netstat/command/CommandNetStatGroupDelete.java
@@ -0,0 +1,41 @@
+package net.hexuscraft.core.netstat.command;
+
+import net.hexuscraft.core.chat.F;
+import net.hexuscraft.core.command.BaseCommand;
+import net.hexuscraft.core.database.PluginDatabase;
+import net.hexuscraft.core.netstat.PluginNetStat;
+import net.hexuscraft.database.queries.ServerQueries;
+import org.bukkit.command.CommandSender;
+import redis.clients.jedis.JedisPooled;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+public class CommandNetStatGroupDelete extends BaseCommand {
+
+    private final PluginDatabase _pluginDatabase;
+
+    CommandNetStatGroupDelete(final PluginNetStat pluginNetStat, final PluginDatabase pluginDatabase) {
+        super(pluginNetStat, "delete", "<Name>", "Delete a server group.", Set.of("del", "d"), PluginNetStat.PERM.COMMAND_NETSTAT_GROUP_DELETE);
+        _pluginDatabase = pluginDatabase;
+    }
+
+    @Override
+    public void run(final CommandSender sender, final String alias, final String[] args) {
+        if (args.length != 1) {
+            sender.sendMessage(help(alias));
+            return;
+        }
+
+        final JedisPooled jedis = _pluginDatabase.getJedisPooled();
+        final String key = ServerQueries.SERVERGROUP(args[0]);
+        sender.sendMessage(F.fMain(this, "Deleted ", F.fItem(key)));
+        jedis.del(key);
+    }
+
+    @Override
+    public List<String> tab(final CommandSender sender, final String alias, final String[] args) {
+        return Arrays.stream(ServerQueries.getServers(_pluginDatabase.getJedisPooled())).map(serverData -> serverData._name).toList();
+    }
+}
