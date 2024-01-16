b1a2ef4d96cefb5fc8095cd39ebdff2e9fe41b80
Render <27890173+itsrenderman@users.noreply.github.com>
2024-01-16 12:42:08 +0000

    Add support for double jump (PluginDoubleJump.java) & custom '_spawn.dat' world data

--- hexuscraft-hub/src/net/hexuscraft/hub/Hub.java
+++ hexuscraft-hub/src/net/hexuscraft/hub/Hub.java
@@ -1,30 +1,48 @@
 package net.hexuscraft.hub;
 
 import net.hexuscraft.core.HexusPlugin;
+import net.hexuscraft.hub.doublejump.PluginDoubleJump;
 import net.hexuscraft.hub.player.PluginPlayer;
 import net.hexuscraft.hub.team.PluginTeam;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.world.WorldLoadEvent;
 
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.util.Scanner;
+
 public class Hub extends HexusPlugin {
+
+    public Location _spawn = new Location(null, 0, 100, 0, 0, 0);
 
     @Override
     public void load() {
+        require(new PluginDoubleJump(this));
         require(new PluginPlayer(this));
         require(new PluginTeam(this));
-
-        getServer().setDefaultGameMode(GameMode.ADVENTURE);
     }
 
     @EventHandler
-    public void on(WorldLoadEvent event) {
-        getServer().setDefaultGameMode(GameMode.ADVENTURE);
-    }
+    public final void onWorldLoad(final WorldLoadEvent event) {
+        final World world = event.getWorld();
+        if (!world.getName().equals("world")) return;
 
-    public Location getSpawn() {
-        return new Location(getServer().getWorld("world"), 0, 100, 0, 0, 0);
+        getServer().setDefaultGameMode(GameMode.ADVENTURE);
+        _spawn.setWorld(world);
+
+        try {
+            final Scanner scanner = new Scanner(Path.of(world.getWorldFolder().getPath(), "_spawn.dat").toFile());
+            _spawn.setX(scanner.nextDouble());
+            _spawn.setY(scanner.nextDouble());
+            _spawn.setZ(scanner.nextDouble());
+            _spawn.setYaw(scanner.nextFloat());
+            _spawn.setPitch(scanner.nextFloat());
+        } catch (FileNotFoundException ex) {
+            log("Could not locate _spawn.dat in world '" + world.getName() + "'");
+        }
     }
 
 }
