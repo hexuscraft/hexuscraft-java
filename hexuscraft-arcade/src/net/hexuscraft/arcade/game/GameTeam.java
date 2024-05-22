b3219544cc783ff174c89452c6610371c87c142f
Render <27890173+itsrenderman@users.noreply.github.com>
2024-05-22 12:55:10 +0100

    Add teams

--- hexuscraft-arcade/src/net/hexuscraft/arcade/game/GameTeam.java
+++ hexuscraft-arcade/src/net/hexuscraft/arcade/game/GameTeam.java
@@ -1,11 +1,13 @@
 package net.hexuscraft.arcade.game;
 
+import org.bukkit.Color;
+
 public class GameTeam {
 
     public final String _name;
-    public final String _color;
+    public final Color _color;
 
-    GameTeam(final String name, final String color) {
+    public GameTeam(final String name, final Color color) {
         _name = name;
         _color = color;
     }
---
+++ hexuscraft-arcade/src/net/hexuscraft/arcade/game/bridge/GameTheBridges.java
@@ -0,0 +1,19 @@
+package net.hexuscraft.arcade.game.bridge;
+
+import net.hexuscraft.arcade.game.Game;
+import net.hexuscraft.arcade.game.GameTeam;
+import org.bukkit.Color;
+
+@SuppressWarnings("unused")
+public class GameTheBridges extends Game {
+
+    protected GameTheBridges() {
+        super("The Bridges", new GameTeam[]{
+                new GameTeam("Red", Color.RED),
+                new GameTeam("Blue", Color.AQUA),
+                new GameTeam("Green", Color.LIME),
+                new GameTeam("Yellow", Color.YELLOW),
+        });
+    }
+
+}
