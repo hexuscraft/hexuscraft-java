36f900a7aaf2faf94bfdbc195c1ee09f8cb33c47
Render <27890173+itsrenderman@users.noreply.github.com>
2024-05-26 23:57:47 +0100

    Extend EntityEvent instead of Event

--- hexuscraft-core/src/net/hexuscraft/core/entity/EntityMoveEvent.java
+++ hexuscraft-core/src/net/hexuscraft/core/entity/EntityMoveEvent.java
@@ -1,63 +1,80 @@
 package net.hexuscraft.core.entity;
 
+import com.avaje.ebean.validation.NotNull;
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.bukkit.event.Cancellable;
-import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+import org.bukkit.event.entity.EntityEvent;
 
 @SuppressWarnings("unused")
-public class EntityMoveEvent extends Event implements Cancellable {
+public class EntityMoveEvent extends EntityEvent implements Cancellable {
 
-    public final Entity _entity;
-    public final Location _from;
-    public final Location _to;
+    private static final HandlerList handlers = new HandlerList();
 
-    private static final HandlerList _handlers = new HandlerList();
-    private boolean _cancelled = false;
+    private boolean cancel = false;
+
+    private final Location _from;
+    private final Location _to;
 
     public EntityMoveEvent(final Entity entity, final Location from, final Location to) {
-        _entity = entity;
+        super(entity);
         _from = from;
         _to = to;
     }
 
     @Override
-    public HandlerList getHandlers() {
-        return _handlers;
-    }
-
-    public HandlerList getHandlerList() {
-        return _handlers;
-    }
-
-    @Override
     public boolean isCancelled() {
-        return _cancelled;
+        return cancel;
     }
 
     @Override
-    public void setCancelled(final boolean cancelled) {
-        _cancelled = cancelled;
+    public void setCancelled(boolean cancel) {
+        this.cancel = cancel;
+    }
+
+    @NotNull
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    @NotNull
+    public HandlerList getHandlerList() {
+        return handlers;
+    }
+
+    public Location getFrom() {
+        return _from;
+    }
+
+    public Location getTo() {
+        return _to;
     }
 
     public boolean isHorizontal(final boolean block) {
-        if (block)
-            return _from.getBlockX() != _to.getBlockX() || _from.getBlockZ() != _to.getBlockZ();
+        if (_from == null) return false;
+        if (_to == null) return false;
+        if (block) return _from.getBlockX() != _to.getBlockX() || _from.getBlockZ() != _to.getBlockZ();
         return _from.getX() != _to.getX() || _from.getZ() != _to.getZ();
     }
 
     public boolean isVertical(final boolean block) {
-        if (block)
-            return _from.getBlockY() != _to.getBlockY();
+        if (_from == null) return false;
+        if (_to == null) return false;
+        if (block) return _from.getBlockY() != _to.getBlockY();
         return _from.getY() != _to.getY();
     }
 
     public boolean isYaw() {
+        if (_from == null) return false;
+        if (_to == null) return false;
         return _from.getYaw() != _to.getYaw();
     }
 
     public boolean isPitch() {
+        if (_from == null) return false;
+        if (_to == null) return false;
         return _from.getPitch() != _to.getPitch();
     }
 
