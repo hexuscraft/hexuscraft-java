d7de4ab51a6d0e683c492b8b3e9e5a9d7c713e3d
Render <27890173+itsrenderman@users.noreply.github.com>
2024-06-09 06:10:33 +0100

    Begin work on cmd /teleport

---
+++ hexuscraft-core/src/net/hexuscraft/core/teleport/command/CommandTeleport.java
@@ -0,0 +1,59 @@
+package net.hexuscraft.core.teleport.command;
+
+import net.hexuscraft.core.HexusPlugin;
+import net.hexuscraft.core.command.BaseCommand;
+import net.hexuscraft.core.teleport.PluginTeleport;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class CommandTeleport extends BaseCommand<HexusPlugin> {
+
+    public CommandTeleport(final PluginTeleport pluginTeleport) {
+        super(pluginTeleport, "teleport", "[Players] <Player/X Y Z>", "Teleport one or more players to a player or coordinates", Set.of("tp"), PluginTeleport.PERM.COMMAND_TELEPORT);
+    }
+
+    @Override
+    public void run(final CommandSender sender, final String alias, final String[] args) {
+
+    }
+
+    @Override
+    public List<String> tab(final CommandSender sender, final String alias, final String[] args) {
+        if (args.length == 1) {
+            final List<String> names = new ArrayList<>(List.of("*", "**"));
+            if (sender instanceof Player player) {
+                names.add(".");
+                names.addAll(_miniPlugin._plugin.getServer().getOnlinePlayers().stream().filter(target -> target.canSee(player)).map(Player::getName).toList());
+                return names;
+            }
+
+            names.addAll(_miniPlugin._plugin.getServer().getOnlinePlayers().stream().map(Player::getName).toList());
+            return names;
+        }
+
+        if (args.length == 2) {
+            final List<String> names = new ArrayList<>(List.of("*", "**"));
+            if (sender instanceof Player player) {
+                names.add(".");
+                names.add(Integer.toString(player.getLocation().getBlockZ()));
+                names.addAll(_miniPlugin._plugin.getServer().getOnlinePlayers().stream().filter(target -> target.canSee(player)).map(Player::getName).toList());
+                return names;
+            }
+
+            return _miniPlugin._plugin.getServer().getOnlinePlayers().stream().map(Player::getName).toList();
+        }
+
+        if (args.length == 3 && sender instanceof Player player)
+            return List.of(Integer.toString(player.getLocation().getBlockY()));
+
+        if (args.length == 4 && sender instanceof Player player)
+            return List.of(Integer.toString(player.getLocation().getBlockZ()));
+
+        return List.of();
+    }
+
+}
