3806da1c51444c18e282edfcbb24517191e70c4b
Render <27890173+itsrenderman@users.noreply.github.com>
2024-06-15 14:47:14 +0100

    Set player GameMode to CREATIVE on join

--- hexuscraft-build/src/net/hexuscraft/build/Build.java
+++ hexuscraft-build/src/net/hexuscraft/build/Build.java
@@ -3,7 +3,10 @@ package net.hexuscraft.build;
 import net.hexuscraft.build.parse.MiniPluginParse;
 import net.hexuscraft.build.world.MiniPluginWorld;
 import net.hexuscraft.core.HexusPlugin;
+import org.bukkit.GameMode;
 import org.bukkit.Location;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class Build extends HexusPlugin {
 
@@ -15,6 +18,11 @@ public class Build extends HexusPlugin {
 
     public Location getSpawn() {
         return new Location(getServer().getWorld("world"), 0, 100, 0, 0, 0);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(final PlayerJoinEvent event) {
+        event.getPlayer().setGameMode(GameMode.CREATIVE);
     }
 
 }
