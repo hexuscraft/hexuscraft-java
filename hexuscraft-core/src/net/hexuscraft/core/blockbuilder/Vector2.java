9902a9305b3e7553f0980cbec1fe3647516f264c
Render <27890173+itsrenderman@users.noreply.github.com>
2024-06-17 12:48:09 +0100

    Update Vector2

--- hexuscraft-core/src/net/hexuscraft/core/blockbuilder/Vector2.java
+++ hexuscraft-core/src/net/hexuscraft/core/blockbuilder/Vector2.java
@@ -1,11 +1,11 @@
 package net.hexuscraft.core.blockbuilder;
 
-public class Vector2 {
+public final class Vector2 {
 
-    public final int _x;
-    public final int _y;
+    public int _x;
+    public int _y;
 
-    Vector2(int x, int y) {
+    public Vector2(int x, int y) {
         _x = x;
         _y = y;
     }
