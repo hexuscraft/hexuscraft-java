d234b387c03f1879301a093da809e886bbc5cac8
Render <27890173+itsrenderman@users.noreply.github.com>
2025-11-18 12:56:37 +0000

    refactor: game prep

---
+++ hexuscraft-arcade/src/net/hexuscraft/arcade/game/GameState.java
@@ -0,0 +1,12 @@
+package net.hexuscraft.arcade.game;
+
+public enum GameState {
+
+    LOADING_MAP,
+    WAITING_FOR_PLAYERS,
+    START_COUNTDOWN,
+    STARTING,
+    IN_PROGRESS,
+    ENDING,
+
+}
--- hexuscraft-arcade/src/net/hexuscraft/arcade/game/GameTeam.java
+++ hexuscraft-arcade/src/net/hexuscraft/arcade/game/GameTeam.java
@@ -2,14 +2,6 @@ package net.hexuscraft.arcade.game;
 
 import org.bukkit.Color;
 
-public final class GameTeam {
-
-    public final String _name;
-    public final Color _color;
-
-    public GameTeam(final String name, final Color color) {
-        _name = name;
-        _color = color;
-    }
+public record GameTeam(String _name, Color _color) {
 
 }
