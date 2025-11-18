99359faef7f9ff36c6166d1a376b17a1f6a7ffec
Render <27890173+itsrenderman@users.noreply.github.com>
2025-11-18 13:05:59 +0000

    refactor: use UUID object instead of String

--- hexuscraft-database/src/net/hexuscraft/database/queries/PermissionQueries.java
+++ hexuscraft-database/src/net/hexuscraft/database/queries/PermissionQueries.java
@@ -2,14 +2,16 @@ package net.hexuscraft.database.queries;
 
 import net.hexuscraft.database.Database;
 
+import java.util.UUID;
+
 public final class PermissionQueries {
 
-    public static String PRIMARY(String uuid) {
-        return Database.buildQuery("user", uuid, "permission", "primary");
+    public static String PRIMARY(final UUID uuid) {
+        return Database.buildQuery("user", uuid.toString(), "permission", "primary");
     }
 
-    public static String GROUPS(String uuid) {
-        return Database.buildQuery("user", uuid, "permission", "groups");
+    public static String GROUPS(final UUID uuid) {
+        return Database.buildQuery("user", uuid.toString(), "permission", "groups");
     }
 
 }
--- hexuscraft-database/src/net/hexuscraft/database/queries/PunishQueries.java
+++ hexuscraft-database/src/net/hexuscraft/database/queries/PunishQueries.java
@@ -2,14 +2,16 @@ package net.hexuscraft.database.queries;
 
 import net.hexuscraft.database.Database;
 
+import java.util.UUID;
+
 public final class PunishQueries {
 
-    public static String LIST(String uuid) {
-        return Database.buildQuery("user", uuid, "punishments");
+    public static String LIST(final UUID uniqueId) {
+        return Database.buildQuery("user", uniqueId.toString(), "punishments");
     }
 
-    public static String PUNISHMENT(String uuid) {
-        return Database.buildQuery("punishment", uuid);
+    public static String PUNISHMENT(final UUID uniqueId) {
+        return Database.buildQuery("punishment", uniqueId.toString());
     }
 
 }
