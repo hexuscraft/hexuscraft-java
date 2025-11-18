945b649af2d8bd8b398ec11155ab732f16faefb7
Render <27890173+itsrenderman@users.noreply.github.com>
2025-11-18 13:05:29 +0000

    refactor: support for username and password in JedisPooled

--- hexuscraft-database/src/net/hexuscraft/database/Database.java
+++ hexuscraft-database/src/net/hexuscraft/database/Database.java
@@ -10,14 +10,15 @@ public final class Database {
 
     public final JedisPooled _jedisPooled;
 
-    public Database(String host, int port) throws Exception {
-        _jedisPooled = new JedisPooled(host, port);
-        Pool<Connection> pool = _jedisPooled.getPool();
+    public Database(final String host, final int port, final String username, final String password) throws Exception {
+        _jedisPooled = new JedisPooled(host, port, username, password);
+
+        final Pool<Connection> pool = _jedisPooled.getPool();
         pool.preparePool();
         assert (_jedisPooled.ping().equals("PONG"));
     }
 
-    public static String buildQuery(String... args) {
+    public static String buildQuery(final String... args) {
         return String.join(KEY_DELIMITER, args);
     }
 
