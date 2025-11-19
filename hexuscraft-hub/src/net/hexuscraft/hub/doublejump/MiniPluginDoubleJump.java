d366a98484fd58dd1666e297b839b25fa8c0ab93
Render <27890173+itsrenderman@users.noreply.github.com>
2025-11-19 01:53:47 +0000

    feat: infinite double jump

--- hexuscraft-hub/src/net/hexuscraft/hub/doublejump/MiniPluginDoubleJump.java
+++ hexuscraft-hub/src/net/hexuscraft/hub/doublejump/MiniPluginDoubleJump.java
@@ -4,10 +4,11 @@ import net.hexuscraft.core.MiniPlugin;
 import net.hexuscraft.hub.Hub;
 import org.bukkit.GameMode;
 import org.bukkit.Sound;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.player.PlayerGameModeChangeEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerToggleFlightEvent;
 
 public final class MiniPluginDoubleJump extends MiniPlugin<Hub> {
@@ -16,26 +17,29 @@ public final class MiniPluginDoubleJump extends MiniPlugin<Hub> {
         super(hub, "Double Jump");
     }
 
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerJoin(final PlayerJoinEvent event) {
+        event.getPlayer().setAllowFlight(true);
+    }
+
     @EventHandler
-    public void onMove(final PlayerMoveEvent event) {
+    public void onGameModeChangedEvent(final PlayerGameModeChangeEvent event) {
         final Player player = event.getPlayer();
 
-        if (!((Entity) player).isOnGround()) return;
+        if (player.getGameMode().equals(GameMode.CREATIVE)) return;
+        if (player.getAllowFlight()) return;
+
         player.setAllowFlight(true);
     }
 
     @EventHandler
     public void onFlight(final PlayerToggleFlightEvent event) {
-        if (event.getPlayer().getGameMode().equals(GameMode.CREATIVE)) return;
+        final Player player = event.getPlayer();
+
+        if (player.getGameMode().equals(GameMode.CREATIVE)) return;
         if (!event.isFlying()) return;
 
         event.setCancelled(true);
-
-        final Player player = event.getPlayer();
-
-        player.setFlying(false);
-        player.setAllowFlight(false);
-
         player.setVelocity(player.getLocation().getDirection().setY(1));
         player.playSound(player.getLocation(), Sound.GHAST_FIREBALL, 1000000, 1);
     }
