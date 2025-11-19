8431e05c689bf51eda39643190a79ac3be43a081
Render <27890173+itsrenderman@users.noreply.github.com>
2025-11-19 02:23:01 +0000

    refactor: add channel name support

--- hexuscraft-core/src/net/hexuscraft/core/database/MessagedRunnable.java
+++ hexuscraft-core/src/net/hexuscraft/core/database/MessagedRunnable.java
@@ -3,22 +3,42 @@ package net.hexuscraft.core.database;
 import net.hexuscraft.core.HexusPlugin;
 import net.hexuscraft.core.MiniPlugin;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 public class MessagedRunnable implements Runnable {
 
     public final MiniPlugin<? extends HexusPlugin> _miniPlugin;
 
-    private String _message;
+    private final AtomicReference<String> _pattern = new AtomicReference<>(null);
+    private final AtomicReference<String> _channelName = new AtomicReference<>(null);
+    private final AtomicReference<String> _message = new AtomicReference<>(null);
 
     protected MessagedRunnable(final MiniPlugin<? extends HexusPlugin> plugin) {
         _miniPlugin = plugin;
     }
 
-    public void setMessage(final String message) {
-        _message = message;
+    public String getPattern() {
+        return _pattern.get();
+    }
+
+    public String getChannelName() {
+        return _channelName.get();
     }
 
     public String getMessage() {
-        return _message;
+        return _message.get();
+    }
+
+    public void setPattern(final String pattern) {
+        _pattern.set(pattern);
+    }
+
+    public void setChannelName(final String channelName) {
+        _channelName.set(channelName);
+    }
+
+    public void setMessage(final String message) {
+        _message.set(message);
     }
 
     @Override
@@ -27,7 +47,8 @@ public class MessagedRunnable implements Runnable {
 
     @Override
     public String toString() {
-        return _miniPlugin._name;
+        System.out.println(this.getClass());
+        return _miniPlugin._prefix;
     }
 
 }
