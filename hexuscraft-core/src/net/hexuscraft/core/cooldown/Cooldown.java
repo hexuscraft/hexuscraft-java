2568589ec7e72723ad888091d9575cf44c60fd3f
Render <27890173+itsrenderman@users.noreply.github.com>
2025-11-19 02:22:09 +0000

    refactor: record-ify

--- hexuscraft-core/src/net/hexuscraft/core/cooldown/Cooldown.java
+++ hexuscraft-core/src/net/hexuscraft/core/cooldown/Cooldown.java
@@ -1,15 +1,5 @@
 package net.hexuscraft.core.cooldown;
 
-public final class Cooldown {
-
-    public final String _name;
-    public final long _started;
-    public final long _delayMs;
-
-    public Cooldown(final String name, final long started, final long delayMs) {
-        _name = name;
-        _started = started;
-        _delayMs = delayMs;
-    }
+public record Cooldown(String _name, long _started, long _delayMs) {
 
 }
