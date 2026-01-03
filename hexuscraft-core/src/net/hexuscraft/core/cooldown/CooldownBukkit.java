9f6e4f5da53795a57ae690e22046dcc0e305f5a7
itsrenderman <27890173+itsrenderman@users.noreply.github.com>
2026-01-03 16:04:22 +0000

    refactor: cooldown bukkit class for auto messages

---
+++ hexuscraft-core/src/net/hexuscraft/core/cooldown/CooldownBukkit.java
@@ -0,0 +1,28 @@
+package net.hexuscraft.core.cooldown;
+
+import net.hexuscraft.common.chat.F;
+import net.hexuscraft.common.utils.UtilCooldown;
+import org.bukkit.command.CommandSender;
+
+public class CooldownBukkit {
+
+    private Long calculateRemaining(final Long now, final Long start, final Long delay) {
+        return delay - (now - start);
+    }
+
+    public boolean use(final Object parent, final String name, final Long delayMs, final CommandSender sender) {
+        if (UtilCooldown.use(parent, name, delayMs)) return true;
+
+        final UtilCooldown.Cooldown cooldown = UtilCooldown.getCooldown(parent, name);
+        if (cooldown == null) {
+            sender.sendMessage(F.fMain(this, "Please wait before trying to use ", F.fItem(name), " again."));
+            return false;
+        }
+
+        sender.sendMessage(F.fMain(this, "You cannot use ", F.fItem(name), " for another ",
+                F.fTime(calculateRemaining(System.currentTimeMillis(), cooldown._started(),
+                        cooldown._delayMs()))));
+        return false;
+    }
+
+}
